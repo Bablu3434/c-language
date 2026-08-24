dead692be4e63648d412941944dedf7e7a14be20
Bablu Raj <babluraj32005@gmail.com>
2026-08-24 21:02:34 +0530

    c language/pic

---
+++ pic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n");
+    printf("              *****\n");
+    printf("            *********\n");
+    printf("           **  ***  **\n");
+    printf("           *  (o o)  *\n");
+    printf("           *    ^    *\n");
+    printf("           *  \\___/  *\n");
+    printf("            \\_______/\n");
+    printf("               |||\n");
+    printf("          =====|||=====\n");
+    printf("               |||\n");
+    printf("            ___|||___\n");
+    printf("           /    |    \\\n");
+    printf("          /_____|_____\\\n");
+    printf("\n");
+    printf("        JAI SHREE KRISHNA\n");
+
+    return 0;
+}
